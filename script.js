b4c966bb7e18773236ec1e71b7b4925e423a953c
Rajee <rajeevan@gmail.com>
2023-10-22 13:18:41 +0530

    New work

---
+++ script.js
@@ -0,0 +1,163 @@
+// console.log("Welcome Uki")
+// document.write("hello");
+// // alert("hi");
+// // window.alert("rajee")
+// document.getElementById("demo").innerHTML = 5 + 6;
+
+
+
+// var firstName="kamal";
+// var lastName="rajan";
+// var x=10;
+// let y=23;
+// console.log(firstName+""+lastName);  
+
+
+// // var can reassign and not redec 
+// let m=10;
+// m=5;
+// console.log(m);
+
+
+// const d=78;
+// // d=45; error
+// console.log(d);
+//---------------------------------------------
+// var x=10;
+// console.log(x+y)
+// var x=15; 
+// var y=20;
+// z=x+y;
+// console.log(z);
+
+// ----------------------------------------------------------------
+// const p=20;{
+//     const p=30;
+//     console.log(p);
+// }
+// console.log(p);
+
+
+// console.log(firstName);
+// var firstName="kamal";
+// var lastName="raja";
+// var x=10;
+// console.log(y);
+// let y=23;
+// // console.log(g);
+// const g=123;
+
+//  var x=5;
+//  var y=6;
+//   z=x<y;
+//  console.log(z);
+
+// let person{
+//     name:Rajeevan,
+//     age :25,
+//     isAlive:true,
+//     gender:""
+//     adder``
+
+// }
+
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.isAlive)
+// console.log()
+
+
+
+
+
+
+// const num1 = 0.1;
+// const num2 = 0.2;
+// const sum = num1 + num2;
+// console.log(sum);  // Result might not be exactly 0.3 due to precision issues.
+
+
+
+
+
+// var firstName="Rajeevan";
+// var greet="hello";
+// console.log(firstName.concat(" ", " ", greet));
+
+// trim-------------------------------------------
+// var a=  "   Hey Uki "   
+// console.log(a.trim());
+
+
+// Replace---------------------------------------------
+// var a= "Good Morning!";
+// var aReplace= a.replace('morning','atternoon');
+
+// console.log(aReplace);
+
+// CharAt------------------------------------------
+// var texti ="Hello gugs";
+// console.log(texti.charAt(3));
+
+
+//slice//
+// str ='The quick brown fox jump over the lazy dog.';
+// console.log(str.split(' ',2));
+
+
+
+
+//array
+/*var fruits=["Banana","Orange","Apple"];
+// fruits[1]= "Car";
+console.log(fruits [1]);
+*/
+
+
+/*
+var  D=[[1,2,3],[6,8,9]]
+console.log(D [1][2]);
+*/
+
+// var A=[]
+// A[99]=100;
+// console.log(A);
+
+
+// //to String 
+// var Number = [1,2,3,4];
+// var Number1 =Number.toString();
+// console.log(Number1);
+
+//join
+/*
+var fruits=["Banana", "Apple", "mango" ];
+var fruits1 = fruits.join (" and ");
+console.log (fruits1);
+*/
+
+
+//pop
+/*
+var fruits2 = ["Banana","Apple", "mango"];
+fruits2.pop();
+console.log(fruits2);
+
+//push
+
+var animals = ["pigs", "dog", "cat"];
+console.log(animals);
+
+var count = animals.push("cows");
+console.log(animals);
+*/
+
+var firstName = "Rajeevan"
+var lastName = "Rajee"
+ 
+console.log (firstName.concat(" ",lastName));
+
+
+let text1 = "Im a"
+let = text2 = "Developer"
+console.log(text1.concat(" ",text2));
